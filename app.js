b87be504e41d8576d642e3d5903bd49f3d313742
SyAtlantis <1102960695@qq.com>
2019-06-26 18:16:55 +0800

    修改打包环境

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ let mainWindow
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 900,
+        height: 700,
+        minWidth: 900,
+        minHeight: 700,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
