35ed93c63b2a911ced833f616b54149c00dfe798
SyAtlantis <1102960695@qq.com>
2019-06-27 14:47:05 +0800

    修改启动项设置

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const { app, BrowserWindow } = require('electron');
+const { fork } = require("child_process");
+const path = require('path');
 
-function createWindow() {
-    // Create the browser window.
+let mainWindow;
+
+let createWindow = () => {
     mainWindow = new BrowserWindow({
         width: 900,
-        height: 700,
+        height: 650,
         minWidth: 900,
-        minHeight: 700,
+        minHeight: 650,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
+    });
 
-    // and load the index.html of the app.
-    mainWindow.loadFile('./publish/index.html')
+    mainWindow.loadFile('./publish/index.html');
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+    mainWindow.on('closed', () => {
         mainWindow = null
-    })
+    });
+
+    // 开启子进程启动服务
+    fork(__dirname + "/server.js", []);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
-app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) createWindow()
-})
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
---
+++ server.js
@@ -0,0 +1,1 @@
+require("etm-starting-server");
